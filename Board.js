63638ed78e01f7ac478b1a13701c542287a7262b
mcg1nley <jjmc.ginley@btinternet.com>
2022-11-13 21:01:17 +0000

    started working on function that takes in user input and then creates tiles based on this

--- Board.js
+++ Board.js
@@ -17,16 +17,32 @@ function createGrid(h, w) {
         container.append(div);
     }
 }
-// Function gets word from input and sends to newWord function - possibly combine these two
+// Get word from user input
+// Put word in to an array of letters
+// Assign each letter to a square and send back to display
 // This function will take in a word from user input and put each letter in to an array as an object
 function newWord(userInput) {
-    // create array from user input
+    // Create array from user input
     const userArray = Array.from(userInput);
-    // create object array for each letter with parameters that we can use to identify the tiles around each letter
+    // Create div element for overall word
+    const wordDiv = document.createElement("div");
+    wordDiv.classList.add("word");
+    // Needs to add an ID to this
+    document.body.appendChild(wordDiv);
+    // Create object array for each letter with parameters that we can use to identify the tiles around each letter
     const word = [];
     // loop each letter of string and map in array
     for (let i = 0; i < userArray.length; i++) {
         const letter = { letter: userArray[i], duplicate: false, up: false, down: false, left: false, right: false };
+        // Create Div element for each tile
+        const letterDiv = document.createElement("div");
+        letterDiv.classList.add("word-square", "empty");
+        // Add ID to this div
+        //letterDiv.setAttribute('id',  )
+        // Append letterDiv to wordDiv
+        //document.getElementById()
+        // Create p element for letter 
+        const letterHTML = document.createElement("p");
         // if first letter in word then left should be false
         if (i !== 0)
             letter.left = true;
@@ -34,11 +50,29 @@ function newWord(userInput) {
         if (i !== (userArray.length - 1))
             letter.right = true;
         word.push(letter);
+        letterHTML.innerText = word[i].letter;
+        letterHTML.classList.add("letter");
+        // Add p element to letterDiv by Id
+        //document.getElementById()
     }
     ;
+    //wordTiles(word);
     console.log(word);
 }
 ;
+/*
+// CREATE WORD TILE - THIS HAS BEEN MIXED WITH newWord function
+function wordTiles(word: {letter: string, duplicate: boolean, up: boolean, down: boolean, left: boolean, right: boolean}[]) {
+  // for every letter in array create a new tile with the content being the letter
+  for (let i = 0; i < word.length; i++) {
+    const para = document.createElement("p") as HTMLElement;
+para.innerText = word[i].letter
+para.classList.add("word-square", "letter")
+document.body.appendChild(para)
+  }
+
+} */
+// Recieves text input from user and sends to function
 function returnText() {
     let input = document.getElementById("userInput").value.toUpperCase();
     newWord(input);
