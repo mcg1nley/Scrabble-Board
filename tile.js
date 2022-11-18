1e5ccd9f79a4101962c634fefc1c5e9ca32c43b3
mcg1nley <jjmc.ginley@btinternet.com>
2022-11-18 20:56:13 +0000

    added tile class

---
+++ tile.js
@@ -0,0 +1,66 @@
+"use strict";
+// How can i reset these values
+class Tile {
+    constructor() {
+        this._empty = true;
+        this._id = " ";
+        this._content = " ";
+        this._up = false;
+        this._right = false;
+        this._down = false;
+        this._left = false;
+        this._duplicate = false;
+    }
+    get empty() {
+        return this._empty;
+    }
+    set empty(Empty) {
+        this._empty = Empty;
+    }
+    get id() {
+        return this._id;
+    }
+    set id(Id) {
+        if (this._empty == true) {
+            console.log("This tile is empty and does not require a specific ID");
+            this._id = " ";
+        }
+        this._id = Id;
+    }
+    get content() {
+        return this._content;
+    }
+    set content(Content) {
+        this._content = Content;
+    }
+    get up() {
+        return this._up;
+    }
+    set up(Up) {
+        this._up = Up;
+    }
+    get right() {
+        return this._right;
+    }
+    set right(Right) {
+        this._right = Right;
+    }
+    get down() {
+        return this._down;
+    }
+    set down(Down) {
+        this._down = Down;
+    }
+    get left() {
+        return this._left;
+    }
+    set left(Left) {
+        this._left = Left;
+    }
+    get duplicate() {
+        return this._duplicate;
+    }
+    set duplicate(Duplicate) {
+        this._duplicate = Duplicate;
+    }
+}
---
+++ tile.ts
@@ -0,0 +1,80 @@
+// How can i reset these values
+class Tile {
+    private _empty: boolean = true;
+    private _id: string = " ";
+    private _content: string = " ";
+    private _up: boolean =false;
+    private _right: boolean =false;
+    private _down: boolean = false;
+    private _left: boolean = false;
+    private _duplicate: boolean = false;
+
+    public get empty() {
+        return this._empty;
+    }
+
+    public set empty(Empty: boolean) {
+        this._empty = Empty;
+    }
+
+    public get id() {
+        return this._id;
+    }
+
+    public set id(Id: string) {
+        if (this._empty == true) {
+            console.log("This tile is empty and does not require a specific ID")
+            this._id = " ";
+        }
+        this._id = Id;
+    }
+
+    public get content() {
+        return this._content;
+    }
+
+    public set content(Content: string) {
+        this._content = Content;
+    }
+
+    public get up() {
+        return this._up;
+    }
+
+    public set up(Up: boolean) {
+        this._up = Up;
+    }
+
+    public get right() {
+        return this._right;
+    }
+
+    public set right(Right: boolean) {
+        this._right = Right;
+    }
+
+    public get down() {
+        return this._down;
+    }
+
+    public set down(Down: boolean) {
+        this._down = Down;
+    }
+
+    public get left() {
+        return this._left;
+    }
+
+    public set left(Left: boolean) {
+        this._left = Left;
+    }
+
+    public get duplicate() {
+        return this._duplicate;
+    }
+
+    public set duplicate(Duplicate: boolean) {
+        this._duplicate = Duplicate;
+    }
+
+}
