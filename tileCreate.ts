794015aacdabd51c2c82b073884b22e4a41fd7df
mcg1nley <jjmc.ginley@btinternet.com>
2022-11-19 12:53:13 +0000

    tried adding UUID

--- tileCreate.ts
+++ tileCreate.ts
@@ -1,10 +1,12 @@
 // This function creates a new tile with its default settings. 
 //No need to change PosX or PosY at this point. 
 // Need to consider adding UUID in order to create a unique ID which can then be used in the CSS file.
+//import {v4 as uuidv4} from "uuid";
 
 export function createNewTile(letter: string, position: string) {
     let tile = new Tile();
     tile.empty = false;
+    //let id: string = uuidv4();
     tile.id = "1234";
     tile.content = letter;
     if (position != "start") {
